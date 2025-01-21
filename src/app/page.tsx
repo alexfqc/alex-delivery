db2c542315eaba73c1dc6727d9daab2bb081c71b
Alessander Franca <alessander70@gmail.com>
2025-01-21 14:48:06 -0300

    main page layout updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,15 @@ import styles from "./page.module.css";
 const Home: FC = () => {
   return (
     <div className={styles.appWrapper}>
-      <div className={styles.appContent}>app</div>
+      <div className={styles.appContent}>
+        <div className={styles.appWelcomeMessage}>
+          Welcome to Alex Delivery&apos;s App
+        </div>
+        <div className={styles.appDescriptionText}>
+          This an app that helps you order food restaurants using AI. Try typing
+          &quot;what are the available restaurants&quot; to get started.
+        </div>
+      </div>
     </div>
   );
 };
